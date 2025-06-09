1e56fad69082fd0e409c9b7126753afb84a177ed
taslimaakter <22101186@uap-bd.edu>
2025-06-09 15:01:16 +0600

    Ready for deployment

--- artist_showcase/settings.py
+++ artist_showcase/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b$odky05!u)2t@xh8=x(d+t@ekr$oheb%%byox)no^urvl-e5e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR/'static',]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
